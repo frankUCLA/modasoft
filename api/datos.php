39cddd3a24c3cefb053761888b143bd61a6921cc
frankUCLA <1003.30928764.ucla@gmail.com>
2025-10-11 18:04:27 +0000

    corregido

--- api/datos.php
+++ api/datos.php
@@ -2,6 +2,15 @@
 // api/datos.php
 // Devuelve filas de la tabla Productos en JSON
 header('Content-Type: application/json; charset=utf-8');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+
+// Responder a preflight OPTIONS
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
 
 // Configuración: ajusta según tu entorno XAMPP
 $dbHost = '127.0.0.1';
